407475440871dc09ddcd7f25d5de2b657e6fe740
leewooyup <zjadlspun1114@naver.com>
2023-07-10 03:57:05 +0900

    세팅

---
+++ src/main/java/com/ll/exam/app20230706/App20230706Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app20230706;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20230706Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App20230706Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/app20230706/App20230706ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app20230706;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20230706ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
