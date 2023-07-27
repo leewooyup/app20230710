9fbc428d05d8a1bf9aee6ba580a8863e13e11d06
leewooyup <zjadlspun1114@naver.com>
2023-07-28 04:21:37 +0900

    서버에서 빌드 성공을 위해 test 프로파일 추가

--- src/test/java/com/ll/exam/app20230706/App20230706ApplicationTests.java
+++ src/test/java/com/ll/exam/app20230706/App20230706ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.exam.app20230706;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class App20230706ApplicationTests {
 
 	@Test
